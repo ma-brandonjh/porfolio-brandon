8d0b2101267c0ff2ae471a7901d4a148b767629e
Brandon Ma <brandonnoob21@gmail.com>
2024-09-26 14:55:18 -0400

    added basic portfolio code

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost (){
+    return( 
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+        </Layout>
+    )
+}
